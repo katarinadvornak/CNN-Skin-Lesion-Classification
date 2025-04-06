2b976ae4fb84a612249eeabbf9a288e01bd1ba5d
Żórawska, Nina (Stud. DACS) <n.zorawska@student.maastrichtuniversity.nl>
2025-04-06 12:21:45 +0000

    Replace neuralnetwork.py

--- CNN/neuralnetwork.py
+++ CNN/neuralnetwork.py
@@ -5,33 +5,9 @@ import numpy as np
 from sklearn.preprocessing import LabelEncoder
 import pickle
 
-def prepare_data(images, labels):
-    # Converting labels (diseases) to categorical format (numbers: for example, 
-    # [disease1, disease2, disease3] will be [0,1,2])
-    label_encoder = LabelEncoder()
-    labels = label_encoder.fit_transform(labels)
-
-    # Save the LabelEncoder to a file after fitting
-    with open('label_encoder.pkl', 'wb') as f:
-        pickle.dump(label_encoder, f)
-
-    # one-hot encoding on the labels, so we are transforming the numerical labels 
-    # into binary vectors, so for example number 1 will be a vector: [0, 1, 0]
-    labels = tf.keras.utils.to_categorical(labels, num_classes=len(label_encoder.classes_))
-    
-    # Splitting the data into training and validation sets (80% is training, 20% is validation)
-    # this means test_size is usually set to 0.2
-    # X_train: The training images. X_val: The validation images. 
-    # y_train: The one-hot encoded labels (vectors) for the training set. y_val: The one-hot encoded labels for the validation set.
-    # label_encoder: transforms the labels (useful if we want to decode predictions back to their original labels).
-
-    X_train, X_val, y_train, y_val = train_test_split(images, labels, test_size=0.2, random_state=42)
-    
-    return X_train, X_val, y_train, y_val, label_encoder
-
 
 # Function to build the CNN model
-def build_model(input_shape, num_classes):
+def build_model(input_shape, num_classes, activation='relu', dropout_rate=0.5, optimizer='adam'):
     model = models.Sequential([
 
         # input layer
@@ -49,7 +25,7 @@ def build_model(input_shape, num_classes):
          #   5. The feature maps are passed to the next layer (e.g., pooling or another convolutional layer).
         #
 
-        layers.Conv2D(32, (3, 3), activation='relu', padding='same'),
+        layers.Conv2D(32, (3, 3), activation=activation, padding='same'),
         # After each convolutional layer we use MaxPooling layers. 
         # They reduce the size of the data (height and width) coming from the convolutional layers. 
         # The output is smaller in size, but the most important features from th original image stays.
@@ -57,10 +33,10 @@ def build_model(input_shape, num_classes):
         # No learnable parameters, jthey just reduce computation time and avoid overfitting
         layers.MaxPooling2D((2, 2)),
         
-        layers.Conv2D(64, (3, 3), activation='relu', padding='same'),
+        layers.Conv2D(64, (3, 3), activation=activation, padding='same'),
         layers.MaxPooling2D((2, 2)),
         
-        layers.Conv2D(128, (3, 3), activation='relu', padding='same'),
+        layers.Conv2D(128, (3, 3), activation=activation, padding='same'),
         layers.MaxPooling2D((2, 2)),
         
         # flatten means that we reshape the 2D vetor of images to a 1D vector 
@@ -73,14 +49,17 @@ def build_model(input_shape, num_classes):
         # X = input from previous layer
         # b = Bias
 
-        layers.Dense(1152, activation='relu'),
+        layers.Dense(1152, activation=activation),
+        layers.Dropout(dropout_rate),  # 50% of neurons randomly turned off
         layers.Dense(num_classes, activation='softmax')
 
     ])
     
-    model.compile(optimizer='adam',
+    model.compile(optimizer=optimizer,
                   loss='categorical_crossentropy',
                   metrics=['accuracy'])
     
+    model.summary()
+
     return model
 
