bdcd83d1d7fff14a277169544de949cce7cef63a
Żórawska, Nina (Stud. DACS) <n.zorawska@student.maastrichtuniversity.nl>
2025-04-06 12:21:28 +0000

    Replace Main.py

--- CNN/Main.py
+++ CNN/Main.py
@@ -5,19 +5,34 @@ from sklearn.preprocessing import LabelEncoder
 import pickle
 from training import metadata_path
 import pandas as pd
-
+import os
 
 # Loading the saved model
-model = load_model('skin_disease_model.h5')
+model = load_model('skin_disease_model128x128.keras')
 
 # Loading the label encoder from the pickle file
 with open('label_encoder.pkl', 'rb') as f:
     label_encoder = pickle.load(f)
 
-# Path to new image to predict
-image_path = '/Users/ninazorawska/Desktop/project 22/HAM10000_images_part_2/ISIC_0029760.jpg'
-image_id = image_path.split("/")[-1].replace(".jpg", "")
-image = load_and_preprocess_image(image_path)
+
+def load_image_by_id(image_id, folder1, folder2):
+    # Construct possible full paths
+    filename = f"{image_id}.jpg"
+    path1 = os.path.join(folder1, filename)
+    path2 = os.path.join(folder2, filename)
+
+    # Check which one exists
+    if os.path.exists(path1):
+        return load_and_preprocess_image(path1)
+    elif os.path.exists(path2):
+        return load_and_preprocess_image(path2)
+    else:
+        raise FileNotFoundError(f"Image {filename} not found in either folder.")
+
+image_id = "ISIC_0025957"
+folder1 = '/Users/ninazorawska/Desktop/project 22/HAM10000_images_part_1'
+folder2 = '/Users/ninazorawska/Desktop/project 22/HAM10000_images_part_2'
+image = load_image_by_id(image_id, folder1, folder2)
 
 
 predictions = model.predict(image)
